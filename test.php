7c758e0005f7114ae7a706c56e73b9c97dbee504
ber34 <ber34@o2.pl>
2021-02-12 19:37:10 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+include("error-logi.class.php");
+ 
+  $logi = new error_logi($_SERVER);
+   
+  $mesage='Błąd log przypisujemy np. błąd pdo lub mysql
+                    albo błąd logowania';
+     $user='Adam ';
+     $html=true;
+   
+  ## ustawiamy kiedy zapisujey logi uzytkownika ##
+    $logi--->user_logi_save($mesage,$user);
+     $logi->user_logi_email($mesage, $user, $html);
+ 
+  ## ustawiamy kiedy zapisujey logi globalne ##
+     $logi->global_logi_save($mesage);
+      $logi->global_logi_email($mesage, $html);
